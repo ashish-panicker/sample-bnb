6cb0bd17257524e3f58c30702dcf0159bcca8e9f
ashish <ashish.s.panicker@proton.me>
2026-02-26 15:25:11 +0530

    Zipkin and Micrometer Tracing, working on prometheus

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,11 +2,14 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
+        // TraceID created at the gateway will be passed down to threads further down the service line
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
